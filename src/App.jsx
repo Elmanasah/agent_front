c5c95086ef3f559c60ddc007c0e5a3f7dc54bd33
hema <hshaabrahym36@gmail.com>
2026-04-03 14:00:44 +0200

    sutting horus down for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
-import AppRoutes from "./routes/AppRoutes";
+// import AppRoutes from "./routes/AppRoutes";
+import Waiting from "./components/Waiting";
 
 export default function App() {
   // don't forget to change the favicon
-  return <AppRoutes />;
+  return(
+    <>
+    {/* <AppRoutes /> */}
+    <Waiting />
+    {/* <div className="w-screen h-screen bg-black"></div> */}
+    </>
+  )
 }
